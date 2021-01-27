2d089c18db49d5091b75f2ad09eaca6d0aeb0fc8
Cuong Nguyen <cuongnt53@gmail.com>
2021-01-27 14:24:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const FormData = require('form-data');
 const express = require('express');
 const axios = require('axios');
 const PORT = process.env.PORT || 7000;
-const ROOT_API = 'https://b0ebc98abdf4.ngrok.io';
+const ROOT_API = 'https://63897b2fae1d.ngrok.io';
 var app = express();
 var bodyParser = require('body-parser');
 
